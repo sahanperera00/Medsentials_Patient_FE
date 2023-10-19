ba55f702b4826de4a480760cd5507347e85832b5
sahan.perera <sahan.perera@rootcodelabs.com>
2023-10-19 19:34:36 +0530

    maps added

--- app/locatePharmacy.jsx
+++ app/locatePharmacy.jsx
@@ -1,23 +1,68 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { router } from "expo-router";
+import { View } from "react-native";
+import React, { useState, useEffect } from "react";
 import tw from "twrnc";
-import Button from "../components/button/Button";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { pharmacies } from "../assets/dummy/pharmacy";
 
-export default function locatePharmacy() {
+export default function LocatePharmacy() {
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    const fetchUserLocation = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Location permission not granted");
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      setPosition({
+        latitude,
+        longitude,
+        latitudeDelta: 0.006,
+        longitudeDelta: 0.006,
+      });
+    };
+
+    fetchUserLocation();
+  }, []);
+
   return (
     <View style={tw`flex-1 items-center px-5`}>
-      <View style={tw`h-30`} />
-      <Text style={tw`text-5xl text-center font-bold`}>Locate Pharmacy</Text>
-      <View style={tw`h-30`} />
-      <View style={tw`flex-1 flex-col w-full gap-5`}>
-        <Button
-          text="Send Request"
-          onPress={() => {
-            router.push("/checkout");
-          }}
-        />
-      </View>
+      {position && (
+        <MapView
+          mapType="standard"
+          userInterfaceStyle="light"
+          style={tw`w-full h-full`}
+          tintColor="#7e57c2"
+          initialRegion={position}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          followsUserLocation={false}
+          showsCompass={true}
+          scrollEnabled={true}
+          zoomEnabled={true}
+          pitchEnabled={false}
+          rotateEnabled={true}
+        >
+          {pharmacies.map((data, index) => {
+            return (
+              <Marker
+              pinColor="#7e57c2"
+                key={index}
+                title={data.name}
+                description={data.description}
+                coordinate={{
+                  latitude: data.latitude,
+                  longitude: data.longitude,
+                }}
+              />
+            );
+          })}
+        </MapView>
+      )}
     </View>
   );
 }
---
+++ assets/dummy/pharmacy.js
@@ -0,0 +1,57 @@
+export const pharmacies = [
+  {
+    _id: 1,
+    name: "Pharmacy A",
+    location: "123 Main Street, City, State",
+    status: "default",
+    phone: "555-123-4567",
+    description:
+      "Pharmacy A provides a wide range of pharmaceutical products and services. We are committed to serving our community with excellence and care.",
+    latitude: 7.0448769670487446,
+    longitude: 79.90889027224887,
+  },
+  {
+    _id: 2,
+    name: "Pharmacy B",
+    location: "123 Main Street, City, State",
+    status: "default",
+    phone: "555-123-4567",
+    description:
+      "Pharmacy B provides a wide range of pharmaceutical products and services. We are committed to serving our community with excellence and care.",
+    latitude: 7.043657788068591,
+    longitude: 79.9084611188472,
+  },
+  {
+    _id: 3,
+    name: "Pharmacy C",
+    location: "123 Main Street, City, State",
+    status: "default",
+    phone: "555-123-4567",
+    description:
+      "Pharmacy C provides a wide range of pharmaceutical products and services. We are committed to serving our community with excellence and care.",
+    latitude: 7.045020712830772,
+    longitude: 79.91006507968592,
+  },
+  {
+    _id: 4,
+    name: "Pharmacy D",
+    location: "123 Main Street, City, State",
+    status: "default",
+    phone: "555-123-4567",
+    description:
+      "Pharmacy D provides a wide range of pharmaceutical products and services. We are committed to serving our community with excellence and care.",
+    latitude: 7.042348098780743,
+    longitude: 79.90740432859559,
+  },
+  {
+    _id: 4,
+    name: "Pharmacy E",
+    location: "123 Main Street, City, State",
+    status: "default",
+    phone: "555-123-4567",
+    description:
+      "Pharmacy E provides a wide range of pharmaceutical products and services. We are committed to serving our community with excellence and care.",
+    latitude: 7.0429177608360725,
+    longitude: 79.90669622548285,
+  },
+];
