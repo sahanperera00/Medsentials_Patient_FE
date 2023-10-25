2c07d9bf8ad5bce321caf2c44cb597a460baf963
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-10-25 11:10:49 +0530

    feat:css changes

--- app/payment.jsx
+++ app/payment.jsx
@@ -3,11 +3,18 @@ import { View, Text, TextInput } from 'react-native';
 import { useRouter } from 'expo-router';
 import tw from 'twrnc';
 import Button from '../components/button/Button';
+import {ScrollView, Dimensions } from "react-native";
+import { router } from "expo-router";
+import PaymentCard from "../components/paymentCard/paymentCard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
+
+const { width } = Dimensions.get("window");
 
 const Payment = () => {
   const router = useRouter();
   const [totalCharges, setTotalCharges] = useState(1500); // Set the total charges based on your data
-
+  const [preferredCard, setPreferredCard] = useState(null);
   const [cardNumber, setCardNumber] = useState('');
 
   const handlePayment = () => {
@@ -21,16 +28,26 @@ const Payment = () => {
   return (
     <View style={tw`flex-1 items-center px-5`}>
       <View style={tw`h-20`} />
-      <Text style={tw`text-5xl text-center font-bold`}>Payment</Text>
+      <Text style={tw`text-4Dexl text-center font-bold`}>Payment</Text>
+      
       <View style={tw`h-10`} />
-      <Text style={tw`text-lg`}>Total Charges: {totalCharges} LKR</Text>
-      <View style={tw`h-10`} />
-      <TextInput
-        style={tw`border rounded-lg p-2 w-full`}
-        placeholder="Enter Card Number"
-        value={cardNumber}
-        onChangeText={(text) => setCardNumber(text)}
-      />
+
+      <PaymentCard
+          style={{
+            width: width - 40,
+            height: 250,
+            borderRadius: 120,
+            resizeMode: "contain",
+          }}
+          isPressed={preferredCard === "card1"}
+          onPress={() => setPreferredCard("card1")}
+        />
+
+<View style={tw`h-10`} />
+        
+      <Text style={tw`text-lg`}>Appointment Charges: {totalCharges} LKR</Text>
+     
+     
       <View style={tw`h-10`} />
       <Button text="Pay Now" onPress={handlePayment} />
     </View>
