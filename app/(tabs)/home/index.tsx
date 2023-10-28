c6d5a37604488435c93d5d5566c168f88cb5f0bf
DevinduSamarasinghe <devindu990@gmail.com>
2023-10-28 11:04:02 +0530

    feat: categories redirect

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,29 +1,20 @@
-import { Text, View, ScrollView } from "react-native";
-import React, { useEffect } from "react";
-import tw from "twrnc";
+import { View, Text } from "react-native";
 import { Link } from "expo-router";
-import Button from "../../../components/button/Button";
-import { router } from "expo-router";
+import React from "react";
+import tw from "twrnc";
+import DocCategoriesList from "../../../components/Appointments/DocCategoriesList";
 
-export default function home() {
+
+export default function categories() {
   return (
-    <ScrollView>
-      <View style={tw`flex-1 items-center px-5`}>
-        <Text style={tw`text-4xl text-center font-bold`}>Home</Text>
-      </View>
-      <View style={tw`flex-1 flex-col w-full gap-5`}>
-        <Link href="/categories">
-          <Text style={tw`text-2xl text-center font-bold`}>Categories</Text>
-        </Link>
+    
 
-        <Button
-          text="Doctors"
-          onPress={() => {
-            router.push("/home/DoctorList");
-          }}
-          style={tw`py-5`}
-        />
-      </View>
-    </ScrollView>
+<View style={tw`flex-1 items-center mt-18`}>
+
+<Link href="DoctorList">
+<Text style={tw`text-3xl text-center font-bold`}>Categories Of Doctors</Text>
+     </Link>
+    < DocCategoriesList/>
+</View>
   );
 }
